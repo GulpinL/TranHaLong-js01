5f0e772320c4fee91dc4ffb5f35a8bb55151a1bd
User <user@QC-Quang.local>
2023-11-15 16:07:26 +0700

    Done all ex01

--- Ex01/1.3-wordCount.js
+++ Ex01/1.3-wordCount.js
@@ -1,4 +1,22 @@
+// Only count upper case Word +1 
+// Need a hold dictionary to check valid word
+// -> use hash or dictionary to check all meaning word
 function CountWordAppearInString(inputStr){
-    // Use hash to check thing in it
-
+    var count = 1;
+    for(var i = inputStr.length;i>0;i--){
+        var _indexChar = inputStr.slice(i-1,i);
+        if(_indexChar === _indexChar.toUpperCase()){
+            count++;
+            // console.log("TRUEEE"+_indexChar);
+        }
+    }
+    return count;
 }
+
+var str1 = "oneTwoThree";// 3
+var str2 = "iLoveUSoMuch";// 5
+
+
+console.log(CountWordAppearInString(str1));
+console.log(CountWordAppearInString(str2));
+
--- Ex01/1.4-getFileExtension.js
+++ Ex01/1.4-getFileExtension.js
@@ -1,10 +1,15 @@
 function GetExtensionOfFile(inputFileNameStr){
-    var _listExtensionFile = ["png", "mp3"];
-    var _log;
-    for(var i = inputFileNameStr.inputFileNameStr;i>0;i--){
-
+    for(var i = inputFileNameStr.length;i>0;i--){
+        var _indexChar = inputFileNameStr.slice(i-1,i);
+        // console.log("io : " + _indexChar);
+        if(_indexChar =="."){
+            return inputFileNameStr.slice(i,inputFileNameStr.length);
+        }
     }
    
 }
 var fileNameStr1 = "image.png";
 var fileNameStr2 = "Sound.mp3";
+
+console.log(GetExtensionOfFile(fileNameStr1));
+console.log(GetExtensionOfFile(fileNameStr2));
